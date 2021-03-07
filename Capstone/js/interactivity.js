46c30d21a273b25f7b97e2a0f0822047f2a1371a
Nourhan Ahmed AboElenen <36276898+nourhanKg@users.noreply.github.com>
2021-03-07 22:35:45 +0200

    Add files via upload

---
+++ Capstone/js/interactivity.js
@@ -0,0 +1,57 @@
+var x = 1;
+function next()
+{
+    if (x < 3 && x > 0)
+    {
+        var slide = x + 1;
+        if (slide == 1)
+        {
+            document.getElementById("slider").style.backgroundImage = "url('../images/group2.jpg')";
+        }
+        else if (slide == 2)
+        {
+            document.getElementById("slider").style.backgroundImage = "url('../images/group3.jpg')";
+        }
+        else if(slide == 3)
+        {
+            document.getElementById("slider").style.backgroundImage = "url('../images/group4.jpg')";
+        }
+        x++;
+    }
+}
+function previous()
+{
+    if (x > 1 && x < 4)
+    {
+        var slide = x - 1;
+        if (slide == 1)
+        {
+            document.getElementById("slider").style.backgroundImage = "url('../images/group2.jpg')";
+        }
+        else if (slide == 2)
+        {
+            document.getElementById("slider").style.backgroundImage = "url('../images/group3.jpg')";
+        }
+        else if(slide == 3)
+        {
+            document.getElementById("slider").style.backgroundImage = "url('../images/group4.jpg')";
+        }
+        x--;
+    }
+}
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+
